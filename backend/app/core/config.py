f50cfa1cb801fa01b169ae9c1c248a2d4be1c568
SpoiledMouse Práce <berretw01@gmail.com>
2025-10-01 09:19:13 +0200

    aa

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ import os
 
 class Settings:
     PROJECT_NAME: str = "HoursService"
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@db:5432/appdb")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@db:5432/appdb")
     JWT_SECRET: str = os.getenv("JWT_SECRET", "dev-secret-change-me")
     JWT_ALG: str = "HS256"
     JWT_EXPIRE_MINUTES: int = int(os.getenv("JWT_EXPIRE_MINUTES", "60"))
